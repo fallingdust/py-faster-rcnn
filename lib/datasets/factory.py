7f780c9cbee1547305e8cf91566f4882da46ea3c
Hongliang Yuan <fallingdust@gmail.com>
2017-02-02 18:48:36 +0800

    Add fruit training examples.

--- lib/datasets/factory.py
+++ lib/datasets/factory.py
@@ -11,6 +11,7 @@ __sets = {}
 
 from datasets.pascal_voc import pascal_voc
 from datasets.coco import coco
+from datasets.fruit import fruit
 import numpy as np
 
 # Set up voc_<year>_<split> using selective search "fast" mode
@@ -30,6 +31,9 @@ for year in ['2015']:
     for split in ['test', 'test-dev']:
         name = 'coco_{}_{}'.format(year, split)
         __sets[name] = (lambda split=split, year=year: coco(split, year))
+
+imageset = 'TRAIN'
+__sets['fruit'] = (lambda imageset=imageset: fruit(imageset))
 
 def get_imdb(name):
     """Get an imdb (image database) by name."""
